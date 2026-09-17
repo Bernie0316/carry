639b0862ebadac2d418e30338591ba5204dfa7d4
Bernie Zhong <chu23008@byui.edu>
2026-09-17 23:39:04 +0800

    add static CSS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.static('public')); // 設定靜態檔案資料夾
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
