1eb115650339021a9d19b311c92e6f70be7a4f06
Bernie Zhong <chu23008@byui.edu>
2026-09-22 21:16:23 +0800

    增加連結到資料庫的功能 遠端render.com 伺服器跟資料庫服務已加入

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const pool = require('./database/database');
+
 const app = express();
+
 app.set('view engine', 'ejs');
+
 app.use(express.static('public')); // 設定靜態檔案資料夾
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     // 打開瀏覽器會出現的訊息
     res.render('home');
+});
+
+
+pool.query("SELECT NOW()", (err, result) => {
+    if (err) {
+        console.error("Database connection error:", err);
+    } else {
+        console.log("Database connected:", result.rows[0]);
+    }
 });
 
 // 開始跑 app.js，並監聽指定的 PORT
---
+++ database/database.js
@@ -0,0 +1,16 @@
+const { Pool } = require("pg");
+// pg 是 Node.js 跟 PostgreSQL溝通的套件
+require("dotenv").config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+module.exports = pool;
